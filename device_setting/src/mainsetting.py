8fdd222e6cefaad09d635ad9bb99610fc88f4853
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2020-10-24 00:04:22 +0700

    UI_device setting
    - Install PyQT5

---
+++ device_setting/src/mainsetting.py
@@ -0,0 +1,41 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication
+import sys
+import setting
+import os
+#membuat pewarisan pada Python, dalam kasus ini class Main mewarisi class MainView
+#langkah ini dilakukan agar kita dapat melakukan modifikasi pada class MainView tanpa harus mengubah pada class tersebut
+#cukup melakukan modifikasi pada class Main
+class Main(QtWidgets.QMainWindow, setting.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        #setupUi merupakan fungsi dari class MainView yang berfungsi untuk melakukan setup
+        #sesuai dengan apa yang telah didefinisikan sebelumnya
+        self.setupUi(self)
+        #jalankan fungsi setValue ketika setButtton diclick
+        self.setButton1.clicked.connect(self.setValue1)
+        self.setButton2.clicked.connect(self.setValue2)
+        self.setButton3.clicked.connect(self.setValue3)
+        self.setButton4.clicked.connect(self.setValue4)
+    #definisikan fungsi setValue
+    def setValue1(self):
+    #set nilai setLabel dengan nilai setTextEdit
+        os.system('roscore')
+
+    def setValue2(self):
+        p='roslaunch turtlebot3_bringup covid_robot.launch'
+        os.system(p)
+    def setValue3(self):
+        q='roslaunch turtlebot3_bringup covid_remote.launch'
+        os.system(q)
+    def setValue4(self):
+        r='roslaunch turtlebot3_navigation covid_navigation.launch'
+        os.system(r)
+def main():
+    app = QApplication(sys.argv)  #buat instance baru QtGui
+    winForm = Main()                    #inisialisasi variable winForm sebagain class Main
+    winForm.show()                      #tampilkan windows form
+    sys.exit(app.exec_())               #eksekusi applikasi
+
+if __name__ == '__main__':              #jika file dijalanakan secara langsung, maka jalankan fungsi main()
+    main()
---
+++ device_setting/src/setting.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'setting.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setGeometry(QtCore.QRect(10, 50, 481, 341))
+        self.tabWidget.setObjectName("tabWidget")
+        self.DeviceSettings = QtWidgets.QWidget()
+        self.DeviceSettings.setObjectName("DeviceSettings")
+        self.setButton1 = QtWidgets.QPushButton(self.DeviceSettings)
+        self.setButton1.setGeometry(QtCore.QRect(140, 10, 181, 51))
+        self.setButton1.setObjectName("setButton1")
+        self.setButton2 = QtWidgets.QPushButton(self.DeviceSettings)
+        self.setButton2.setGeometry(QtCore.QRect(140, 80, 181, 51))
+        self.setButton2.setObjectName("setButton2")
+        self.setButton3 = QtWidgets.QPushButton(self.DeviceSettings)
+        self.setButton3.setGeometry(QtCore.QRect(140, 160, 181, 51))
+        self.setButton3.setObjectName("setButton3")
+        self.setButton4 = QtWidgets.QPushButton(self.DeviceSettings)
+        self.setButton4.setGeometry(QtCore.QRect(140, 230, 181, 51))
+        self.setButton4.setObjectName("setButton4")
+        self.tabWidget.addTab(self.DeviceSettings, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.setButton1.setText(_translate("MainWindow", "ROS Master"))
+        self.setButton2.setText(_translate("MainWindow", "Robot Init"))
+        self.setButton3.setText(_translate("MainWindow", "Teleoperation Mode"))
+        self.setButton4.setText(_translate("MainWindow", "Navigation Mode"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.DeviceSettings), _translate("MainWindow", "Device Settings"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+
