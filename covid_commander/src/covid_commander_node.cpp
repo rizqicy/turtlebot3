b0ca55e15b0b37b466b28de3cf3103bca8af47ca
Dimasyoga <dimasyogapra@gmail.com>
2020-11-17 07:38:09 +0700

    add payload msgs handle

--- covid_commander/src/covid_commander_node.cpp
+++ covid_commander/src/covid_commander_node.cpp
@@ -5,24 +5,28 @@
 #include <geometry_msgs/Pose.h>
 #include <std_msgs/String.h>
 #include <std_msgs/UInt8.h>
+#include <std_msgs/UInt16.h>
 #include <std_msgs/Bool.h>
 #include <std_srvs/Trigger.h>
 #include <service_robot_msgs/Command.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 geometry_msgs::Pose pose;
-std_msgs::UInt8 payload_msg;
+geometry_msgs::Pose base_position;
+std_msgs::UInt8 delivery;
+std_msgs::UInt16 payload_msg;
 std_msgs::Bool status;
 bool newMsg = false;
 bool move_done = false;
 bool payload_done = false;
 bool active = false;
 bool cancelFlag = false;
+bool waitPayload = false;
 
 // Called every time feedback is received for the goal
 void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback)
 {
-//   do something?
+    base_position = feedback->base_position.pose;
 }
 
 // Called once when the goal completes
@@ -43,13 +47,18 @@ void activeCb()
 
 void commandCallback(service_robot_msgs::Command msg){
     pose = msg.coordinate;
-    payload_msg = msg.num;
+    delivery = msg.num;
     ROS_INFO("New Command");
     newMsg = true;
 }
 
-void payloadCallback(std_msgs::String msg){
-    payload_done = true;
+void payloadCallback(std_msgs::UInt16 msg){
+    if (waitPayload){
+        if (((msg.data & 0b0000000011111111) ^ (msg.data >> 8)) == 0b0000000000000000){
+            payload_done = true;
+            waitPayload = false;
+        }
+    }
 }
 
 bool cancelCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res){
@@ -60,39 +69,49 @@ bool cancelCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response
     return true;
 }
 
-uint16_t payload_cmd_gen(std_msgs::UInt8 num, uint8_t flags){
+std_msgs::UInt16 payload_cmd_gen(uint8_t num, uint8_t flags){
+    std_msgs::UInt16 ret;
     if (flags == 0){        //open door
-        return 0b00000000100010;
+        ret.data = 0b0000000000100010;
     }else if (flags == 1){  //close door
-        return 0b00000000110010;
+        ret.data = 0b0000000000110010;
     }else if (flags == 2){  //select led on
-
+        uint16_t msg = 0b0000000000010000;
+        for (uint8_t i=1;i<num;i++){
+            msg << 1;
+        }
+        ret.data = msg || 0b0000000000000001;
     }else if (flags == 3){  //all led off
-
+        ret.data = 0b0000000000000001;
     }else{
-
+        ret.data = 0b0000000000000000;
     }
+
+    return ret;
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "covid_commander");
     ros::NodeHandle n;
-    ros::Subscriber command_sub = n.subscribe("command_topic", 1, commandCallback);
-    ros::Subscriber payload_sub = n.subscribe("payloadFB_topic", 1, payloadCallback);
-    ros::Publisher status_pub = n.advertise<std_msgs::Bool>("status_topic", 1);
-    ros::Publisher payload_pub = n.advertise<std_msgs::UInt8>("payload_topic", 1);
+    ros::Subscriber command_sub = n.subscribe("command_topic", 10, commandCallback);
+    ros::Subscriber payload_sub = n.subscribe("payload_status", 10, payloadCallback);
+    ros::Publisher status_pub = n.advertise<std_msgs::Bool>("status_topic", 10);
+    ros::Publisher payload_pub = n.advertise<std_msgs::UInt16>("payload_cmd", 10);
+    ros::Publisher base_position_pub = n.advertise<geometry_msgs::Pose>("position_topic", 10);
     ros::ServiceServer service = n.advertiseService("cancel_task", cancelCallback);
 
+    ros::Rate r(50);
     //tell the action client that we want to spin a thread by default
     MoveBaseClient ac(n, "move_base", true);
     //wait for the action server to come up
-    // while (!ac.waitForServer(ros::Duration(5.0))){
-    //     ROS_INFO("Waiting for the move_base action server to come up");
-    // }
+    while (!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
 
     while (ros::ok()){
         
         ros::spinOnce();
+        r.sleep();
 
         if ((newMsg) && (!active)){
             newMsg = false;
@@ -108,11 +127,19 @@ int main(int argc, char** argv){
             ac.sendGoal(goal, &doneCb, &activeCb, &feedbackCb);
         }
 
+        if (active){
+            base_position_pub.publish(base_position);
+        }
+
         if (move_done){
             move_done = false;
+            payload_msg = payload_cmd_gen(0, 0);
+            payload_pub.publish(payload_msg);
             if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
                 ROS_INFO("Goal reached, publish payload msgs");
+                payload_msg = payload_cmd_gen(delivery.data, 2);
                 payload_pub.publish(payload_msg);
+                waitPayload = true;
             }else{
                 ROS_WARN("Failed to reach position");
                 status.data = false;
@@ -122,6 +149,8 @@ int main(int argc, char** argv){
         }
 
         if (payload_done){
+            payload_msg = payload_cmd_gen(0, 1);
+            payload_pub.publish(payload_msg);
             payload_done = false;
             active = false;
             status.data = true;
